a7a8b68a6648060092d9861296633d4c01acc7af
Walter Galan <mr2incredible@gmail.ccom>
2016-08-03 12:52:28 -0700

    First commit

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Walter Galan on 1/30/16.
+//  Copyright © 2016 Walter Galan. All rights reserved.
+//
+//  Written in Vim
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+  printf("Hello, World!\n");
+
+  return 0;
+}
+
